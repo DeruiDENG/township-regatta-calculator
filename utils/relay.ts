bf282f14418e39556c01c97423645e350fbb0917
DeruiDENG <moonface.ddr@gmail.com>
2021-06-27 09:31:23 +0800

    Core feature in progress.

--- utils/relay.ts
+++ utils/relay.ts
@@ -1,6 +1,7 @@
 const MaxTaskPerPlayer = 16;
 const NormalTaskScore = 135;
 const RelayTaskMinScore = 130;
+const RelayTaskStep = 5;
 const RelayTaskMaxScore = 145;
 
 export function isFullScore(numOfPlayers: number, score: number): boolean {
@@ -13,4 +14,28 @@ export function isFullScore(numOfPlayers: number, score: number): boolean {
   }
 
   return false;
+}
+
+function getSingleTaskScore(numOfPlayersDone: number): number {
+  if (numOfPlayersDone === 1) {
+    return RelayTaskMinScore;
+  }
+
+  if (numOfPlayersDone === 2) {
+    return RelayTaskMinScore * 2 + RelayTaskStep;
+  }
+
+  if (numOfPlayersDone === 3) {
+    return RelayTaskMinScore * 3 + RelayTaskStep * 3;
+  }
+
+  return (
+    RelayTaskMinScore * 3 +
+    RelayTaskStep * 3 +
+    RelayTaskMaxScore * (numOfPlayersDone - 3)
+  );
+}
+
+function getTaskPossibleScores(numOfPlayers: number): number[] {
+  return [];
 }
