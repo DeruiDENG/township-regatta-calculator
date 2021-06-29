ddc40bfce17eee4a21096481ed56f0ade7b96be3
DeruiDENG <moonface.ddr@gmail.com>
2021-06-30 01:16:37 +0800

    partially working solution.

--- utils/relay.test.ts
+++ utils/relay.test.ts
@@ -1,56 +1,44 @@
-import { isFullScore } from './relay';
+import { parse } from './relay';
 
 describe('#analysis', () => {
   test('should work for one task in parallel', () => {
-    expect(isFullScore(5, 0)).toBe(true);
-    expect(isFullScore(5, 130)).toBe(true);
-    expect(isFullScore(5, 130 + 135)).toBe(true);
+    expect(parse(5, 0).isFullScore).toBe(true);
+    expect(parse(5, 130).isFullScore).toBe(true);
+    expect(parse(5, 130 + 135).isFullScore).toBe(true);
 
-    expect(isFullScore(5, 135 + 135)).toBe(false);
-    expect(isFullScore(5, 135 + 133)).toBe(false);
+    expect(parse(5, 135 + 135).isFullScore).toBe(false);
+    expect(parse(5, 135 + 133).isFullScore).toBe(false);
 
-    expect(isFullScore(5, 130 + 135 + 140)).toBe(true);
-    expect(isFullScore(5, 130 + 135 + 140 + 145)).toBe(true);
-    expect(isFullScore(5, 130 + 135 + 140 + 145 * 5)).toBe(false);
-  });
-
-  test('should work for small number of players', () => {
-    expect(isFullScore(1, 135)).toBe(true);
-    expect(isFullScore(1, 135 * 16)).toBe(true);
-    expect(isFullScore(1, 135 * 17)).toBe(false);
-    expect(isFullScore(1, 135 + 130)).toBe(false);
-    expect(isFullScore(1, 130)).toBe(false);
-
-    expect(isFullScore(2, 130 + 135)).toBe(false);
-    expect(isFullScore(2, 130 + 130)).toBe(false);
-    expect(isFullScore(2, 135 + 135)).toBe(true);
-    expect(isFullScore(2, 135)).toBe(true);
-
-    expect(isFullScore(3, 135)).toBe(true);
-    expect(isFullScore(3, 135 * 2)).toBe(true);
-    expect(isFullScore(3, 135 * 3)).toBe(true);
-    expect(isFullScore(3, 135 * 16 * 3)).toBe(true);
-    expect(isFullScore(3, 130)).toBe(false);
+    expect(parse(5, 130 + 135 + 140).isFullScore).toBe(true);
+    expect(parse(5, 130 + 135 + 140 + 145).isFullScore).toBe(true);
+    expect(parse(5, 130 + 135 + 140 + 145 * 5).isFullScore).toBe(false);
   });
 
   test('should work for two tasks in parallel', () => {
-    expect(isFullScore(5, 130 + 130)).toBe(true);
-    expect(isFullScore(5, 130 + 130 + 135)).toBe(true);
-    expect(isFullScore(5, 130 + 135 + 140 + 145 + 130 + 135)).toBe(true);
+    expect(parse(6, 130 + 130).isFullScore).toBe(true);
+    expect(parse(6, 130 + 130 + 135).isFullScore).toBe(true);
+    expect(parse(6, 130 + 135 + 140 + 145 + 130 + 135).isFullScore).toBe(true);
   });
 
   test('should handle impossible number of tasks in parallel', () => {
-    expect(isFullScore(5, 130 * 5)).toBe(false);
-    expect(isFullScore(5, 130 * 1)).toBe(true);
+    expect(parse(5, 130 * 5).isFullScore).toBe(false);
+    expect(parse(5, 130).isFullScore).toBe(true);
 
-    expect(isFullScore(21, 130 * 4)).toBe(true);
-    expect(isFullScore(21, 130)).toBe(true);
-    expect(isFullScore(21, 130 * 5)).toBe(false);
+    expect(parse(21, 130 * 4).isFullScore).toBe(true);
+    expect(parse(21, 130).isFullScore).toBe(true);
+    expect(parse(21, 130 * 5).isFullScore).toBe(false);
 
-    expect(isFullScore(20, 130 * 3)).toBe(true);
-    expect(isFullScore(20, 130 * 3 + 135)).toBe(true);
-    expect(isFullScore(20, 130)).toBe(true);
-    expect(isFullScore(20, 130 * 4)).toBe(false);
-    expect(isFullScore(20, 130 * 4 + 135)).toBe(false);
+    expect(parse(20, 130 * 3).isFullScore).toBe(true);
+    expect(parse(20, 130 * 3 + 135).isFullScore).toBe(true);
+    expect(parse(20, 130).isFullScore).toBe(true);
+    expect(parse(20, 130 * 4).isFullScore).toBe(false);
+    expect(parse(20, 130 * 4 + 135).isFullScore).toBe(false);
+  });
+
+  test('should work for random data', () => {
+    expect(parse(20, 4000).isFullScore).toBe(true);
+    // expect(parse(20, 17740).isFullScore).toBe(true);
+    // expect(parse(20, 6425).isFullScore).toBe(true);
+    // expect(parse(20, 6035).isFullScore).toBe(true);
   });
 });
--- utils/relay.ts
+++ utils/relay.ts
@@ -1,32 +1,16 @@
-const MaxTaskPerPlayer = 16;
-const NormalTaskScore = 135;
 const RelayTaskMinScore = 130;
 const RelayTaskStep = 5;
 const RelayTaskMaxScore = 145;
 
-export function isFullScore(numOfPlayers: number, score: number): boolean {
-  if (numOfPlayers <= 3) {
-    return (
-      score % 135 === 0 &&
-      score >= 0 &&
-      score <= numOfPlayers * MaxTaskPerPlayer * NormalTaskScore
-    );
-  }
-
-  return false;
-}
-
 function getSingleTaskScore(numOfPlayersDone: number): number {
-  if (numOfPlayersDone === 1) {
-    return RelayTaskMinScore;
-  }
-
-  if (numOfPlayersDone === 2) {
-    return RelayTaskMinScore * 2 + RelayTaskStep;
-  }
-
-  if (numOfPlayersDone === 3) {
-    return RelayTaskMinScore * 3 + RelayTaskStep * 3;
+  if (numOfPlayersDone <= 3) {
+    return (
+      ((RelayTaskMinScore +
+        RelayTaskMinScore +
+        (numOfPlayersDone - 1) * RelayTaskStep) *
+        numOfPlayersDone) /
+      2
+    );
   }
 
   return (
@@ -36,6 +20,71 @@ function getSingleTaskScore(numOfPlayersDone: number): number {
   );
 }
 
-function getTaskPossibleScores(numOfPlayers: number): number[] {
-  return [];
+type RegattaStatus = number[];
+
+export function parse(
+  numOfPlayers: number,
+  points: number
+): { isFullScore: boolean; matches: RegattaStatus[] } {
+  const maxNumberOfInprogressTask = getMaxNumberOfInprogressTask(numOfPlayers);
+  const queue: RegattaStatus[] = [Array(16).fill(0)];
+  const results: RegattaStatus[] = [];
+  while (queue.length) {
+    const combination = queue.pop() as number[];
+    const sum = combination.reduce(
+      (acc, cur) => acc + getSingleTaskScore(cur),
+      0
+    );
+    if (sum === points) {
+      results.push(combination);
+    } else if (sum > points) {
+      continue;
+    }
+
+    const nextStatus = getNextStatus(
+      combination,
+      numOfPlayers,
+      maxNumberOfInprogressTask
+    );
+    queue.unshift(...nextStatus);
+  }
+
+  console.log(`Input: ${numOfPlayers} ${points}`);
+  console.log('Output: ', results);
+  return { isFullScore: results.length > 0, matches: results };
+}
+
+function getNextStatus(
+  currentStatus: RegattaStatus,
+  numOfPlayers: number,
+  maxNumberOfInprogress: number
+): RegattaStatus[] {
+  return currentStatus
+    .map((task, index) => {
+      const newTask = [...currentStatus];
+      newTask[index]++;
+      return newTask;
+    })
+    .filter((nextStatus) => {
+      const isOrdered = nextStatus.every(
+        (val, index, array) => index === 0 || val <= array[index - 1]
+      );
+      const isInScope = nextStatus.every((val) => val <= numOfPlayers);
+      const inprogressCount = nextStatus.filter(
+        (taskStatus) => taskStatus < numOfPlayers && taskStatus > 0
+      ).length;
+      return isOrdered && isInScope && inprogressCount <= maxNumberOfInprogress;
+    });
+}
+
+function getMaxNumberOfInprogressTask(numOfPlayers: number): number {
+  if (numOfPlayers >= 21) {
+    return 4;
+  } else if (numOfPlayers >= 11) {
+    return 3;
+  } else if (numOfPlayers >= 6) {
+    return 2;
+  }
+
+  return 1;
 }
